d2e93a0ae22071f57c7200830dac81d70ad9de7e
Siddharth <sid@ava.ri.cmu.edu>
2020-05-21 22:26:32 -0400

    empty

--- experiment_lava_terminal.sh
+++ experiment_lava_terminal.sh
@@ -1,7 +1,7 @@
-env=lava
+env=empty
 print_config=''
-rollout_hint=LavaPPO
-model_name=MiniGrid-LavaCrossingS9N0-v0
+rollout_hint=PPO
+model_name=MiniGrid-Empty-6x6-v2
 
 ############################
 ## In case you forget: model_name: EmptyGAIL
@@ -14,10 +14,10 @@ do
     # This is for BC
     #python -m imitation.scripts.train_adversarial ${print_config} with ${env} rollout_hint=${rollout_hint} gail seed=${seed} model_name=${model_name}bc bc 2> logs/exp1_${model_name}bc_${seed}.txt &
     # This is for positive reward
-#    python -m imitation.scripts.train_adversarial ${print_config} with ${env} rollout_hint=${rollout_hint} gail \
-#    seed=${seed} model_name=${model_name}negative total_timesteps=1100000 negative_reward
+    python -m imitation.scripts.train_adversarial ${print_config} with ${env} rollout_hint=${rollout_hint} gail \
+    seed=${seed} model_name=${model_name}negative_check total_timesteps=1100000 negative_reward
 
     python -m imitation.scripts.train_adversarial ${print_config} with ${env} rollout_hint=${rollout_hint} gail \
-    seed=${seed} model_name=${model_name}negative_terminal total_timesteps=1100000 negative_reward use_terminal_state_disc
+    seed=${seed} model_name=${model_name}negative_terminal_check2 total_timesteps=1100000 negative_reward use_terminal_state_disc
 
 done
