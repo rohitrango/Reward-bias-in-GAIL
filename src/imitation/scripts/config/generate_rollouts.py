8d203dd1ff15f5f3b90f60b21ee865c7f0582424
Siddharth <sid@ava.ri.cmu.edu>
2020-07-11 01:31:39 -0400

    generating rollouts

---
+++ src/imitation/scripts/config/generate_rollouts.py
@@ -0,0 +1,645 @@
+"""Configuration for imitation.scripts.train_adversarial."""
+
+import os
+
+import sacred
+from stable_baselines.common import policies
+
+from imitation import util
+from imitation.policies import base
+from imitation.scripts.config.common import DEFAULT_INIT_RL_KWARGS
+from stable_baselines.common.policies import minigrid_extractor_small, minigrid_extractor
+
+train_ex = sacred.Experiment("generate_rollouts", interactive=True)
+
+@train_ex.config
+def train_defaults():
+  env_name = "CartPole-v1"  # environment to train on
+  total_timesteps = 1e6  # Num of environment transitions to sample
+  max_episode_steps = None
+  # parallel = True
+  parallel = False
+  num_vec = 1
+  n_expert_demos = None  # Num demos used. None uses every demo possible
+  n_episodes_eval = 50  # Num of episodes for final mean ground truth return
+  airl_entropy_weight = 1.0
+  model_name = None
+  assert model_name is not None
+
+  normalize = True  # Use VecNormalize
+  normalize_kwargs = dict()  # kwargs for `VecNormalize`
+  dac = False
+  rollout_save_n_episodes = 1000  # Num episodes saved per file, optional.
+  rollout_save_n_timesteps = None  # Min timesteps saved per file, optional.
+
+  # Number of epochs in between plots (<0 disables) (=0 means final plot only)
+  plot_interval = -1
+  n_plot_episodes = 5  # Number of rollouts for each mean_ep_rew data
+  # Interval for extra episode rew data. (<=0 disables)
+  extra_episode_data_interval = -1
+  show_plots = True  # Show plots in addition to saving them
+
+
+  init_trainer_kwargs = dict(
+      # num_vec=8,  # Must evenly divide gen_batch_size
+      num_vec=1,  # Must evenly divide gen_batch_size
+      parallel=True,  # Use SubprocVecEnv (generally faster if num_vec>1)
+      max_episode_steps=None,  # Set to positive int to limit episode horizons
+      scale=True,
+      reward_kwargs=dict(
+          theta_units=[32, 32],
+          phi_units=[32, 32],
+      ),
+      use_bc = False,
+      init_rl_kwargs=dict(policy_class=base.FeedForward32Policy,
+                          **DEFAULT_INIT_RL_KWARGS),
+      discrim_kwargs=dict(reward_type='positive'),
+  )
+
+  log_root = os.path.join('/serverdata/sid/reward_bias/imitation', "output", "train_adversarial")  # output directory
+  checkpoint_interval = 0  # num epochs between checkpoints (<0 disables)
+  init_tensorboard = False  # If True, then write Tensorboard logs.
+  rollout_hint = None  # Used to generate default rollout_path
+  data_dir = "data/"  # Default data directory
+
+  # `gen_batch_size` must be a multiple of `init_trainer_kwargs.num_vec`.
+  # (If using PPO2, then also must be a multiple of
+  # `init_trainer_kwargs.init_rl_kwargs.nminibatch`).
+  disc_batch_size = 2048*8  # Batch size for discriminator updates.
+  disc_minibatch_size = 512  # Num discriminator updates per batch
+  gen_batch_size = 2048*8  # Batch size for generator updates.
+
+@train_ex.named_config
+def noptepochs4():
+  init_trainer_kwargs = dict(
+      init_rl_kwargs=dict(noptepochs=4),
+  )
+
+@train_ex.named_config
+def dac():
+  dac=True
+
+@train_ex.config
+def aliases_default_gen_batch_size(gen_batch_size):
+  # Setting generator buffer capacity and discriminator batch size to
+  # the same number is equivalent to not using a replay buffer at all.
+  # "Disabling" the replay buffer seems to improve convergence speed, but may
+  # come at a cost of stability.
+  gen_replay_buffer_size = gen_batch_size  # Num generator transitions stored
+
+
+@train_ex.config
+def apply_init_trainer_kwargs_aliases(disc_minibatch_size,
+                                      disc_batch_size,
+                                      gen_replay_buffer_size):
+  init_trainer_kwargs = dict(
+    trainer_kwargs=dict(
+      disc_minibatch_size=disc_minibatch_size,
+      gen_replay_buffer_capacity=gen_replay_buffer_size,
+      disc_batch_size=disc_batch_size,
+    ))
+
+
+@train_ex.config
+def calc_n_steps(init_trainer_kwargs, gen_batch_size):
+  _num_vec = init_trainer_kwargs["num_vec"]
+  assert gen_batch_size % _num_vec == 0, ("num_vec must evenly divide "
+                                          "gen_batch_size")
+  init_trainer_kwargs["init_rl_kwargs"]["n_steps"] = gen_batch_size // _num_vec
+  del _num_vec
+
+
+@train_ex.config
+def paths(env_name, model_name, log_root, rollout_hint, data_dir, seed):
+  log_dir = os.path.join(log_root, env_name.replace('/', '_'), model_name, str(seed))
+
+  # Recommended that user sets rollout_path manually.
+  # By default we guess the named config associated with `env_name`
+  # and attempt to load rollouts from `data/expert_models/`.
+  _d_root = os.path.join('/serverdata/sid/reward_bias/imitation', "output", "expert_demos")  # output directory
+  if rollout_hint is None:
+    rollout_hint = env_name.split("-")[0].lower()
+  rollout_path = os.path.join(_d_root,
+                              env_name,
+                              f"{rollout_hint}",
+                              "rollouts", "final.pkl")
+  assert os.path.exists(rollout_path), rollout_path
+
+
+# Training algorithm named configs
+
+@train_ex.named_config
+def gail():
+  init_trainer_kwargs = dict(
+      use_gail=True,
+  )
+
+@train_ex.named_config
+def bc():
+    init_trainer_kwargs = dict(use_bc=True)
+    total_timesteps = 100
+
+@train_ex.named_config
+def notraj():
+    init_trainer_kwargs = dict(
+            trainer_kwargs=dict(
+                disc_opt_kwargs=dict(
+                        learning_rate=0,
+            )
+        ))
+
+
+@train_ex.named_config
+def airl():
+  init_trainer_kwargs = dict(
+      use_gail=False,
+  )
+
+
+# Shared settings
+
+MUJOCO_SHARED_LOCALS = dict(
+  init_trainer_kwargs=dict(
+    airl_entropy_weight=0.1,
+  ),
+)
+
+ANT_SHARED_LOCALS = dict(
+  total_timesteps=3e7,
+  gen_batch_size=2048*8,
+  disc_batch_size=2048*8,
+  #init_trainer_kwargs=dict(
+    #max_episode_steps=500,  # To match `inverse_rl` settings.
+  #),
+)
+
+
+# Classic RL Gym environment named configs
+
+@train_ex.named_config
+def acrobot():
+  env_name = "Acrobot-v1"
+  rollout_hint = "acrobot"
+
+
+@train_ex.named_config
+def cartpole():
+  env_name = "CartPole-v1"
+  rollout_hint = "cartpole"
+  init_trainer_kwargs = dict(
+      scale=False,
+  )
+
+
+@train_ex.named_config
+def mountain_car():
+  env_name = "MountainCar-v0"
+  rollout_hint = "mountain_car"
+
+
+@train_ex.named_config
+def pendulum():
+  env_name = "Pendulum-v0"
+  rollout_hint = "pendulum"
+
+
+# Standard MuJoCo Gym environment named configs
+
+@train_ex.named_config
+def ant():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  locals().update(**ANT_SHARED_LOCALS)
+  env_name = "Ant-v2"
+  rollout_hint = "ant"
+
+@train_ex.named_config
+def fetchreach():
+    env_name = 'FetchReach-v1'
+    total_timesteps=int(5e5)
+
+@train_ex.named_config
+def half_cheetah():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  env_name = "HalfCheetah-v2"
+  rollout_hint = "half_cheetah"
+  total_timesteps = 2e6
+
+
+@train_ex.named_config
+def hopper():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  # TODO(adam): upgrade to Hopper-v3?
+  env_name = "Hopper-v2"
+  rollout_hint = "hopper"
+  total_timesteps = 2e6
+
+
+@train_ex.named_config
+def humanoid():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  env_name = "Humanoid-v2"
+  rollout_hint = "humanoid"
+  total_timesteps = 4e6
+
+
+@train_ex.named_config
+def reacher():
+  env_name = "Reacher-v2"
+  rollout_hint = "reacher"
+
+
+@train_ex.named_config
+def swimmer():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  env_name = "Swimmer-v2"
+  rollout_hint = "swimmer"
+  total_timesteps = 2e6
+  init_trainer_kwargs = dict(
+      init_rl_kwargs=dict(
+          policy_network_class=policies.MlpPolicy,
+      ),
+  )
+
+
+@train_ex.named_config
+def walker():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  env_name = "Walker2d-v2"
+  rollout_hint = "walker"
+  total_timesteps = 2e6
+
+@train_ex.named_config
+def emptyv1():
+    total_timesteps = int(1e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-Empty-6x6-v1'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='EmptyPPO'
+    normalize=False
+
+@train_ex.named_config
+def emptyv2():
+    total_timesteps = int(1e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-Empty-6x6-v2'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='EmptyPPO'
+    normalize=False
+
+@train_ex.named_config
+def empty():
+    total_timesteps = int(2e5)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-Empty-Random-6x6-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='EmptyPPO'
+    normalize=False
+
+@train_ex.named_config
+def doorkey():
+    rollout_save_n_episodes = 1000  # Num episodes saved per file, optional.
+    rollout_save_n_timesteps = None  # Min timesteps saved per file, optional.
+
+    total_timesteps = int(1e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-DoorKey-6x6-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='DoorKeyPPO'
+    normalize=False
+
+@train_ex.named_config
+def redblue():
+    total_timesteps=int(1e7)
+    gen_batch_size = 2048 * 16
+    env_name = 'MiniGrid-RedBlueDoors-6x6-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='RedBluePPO'
+    normalize=False
+# Debug configs
+
+@train_ex.named_config
+def gotodoor():
+    total_timesteps=int(5e6)
+    gen_batch_size = 2048 * 8
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='GoToDoorPPO'
+    normalize=False
+    env_name = 'MiniGrid-GoToDoor-6x6-v0'
+
+@train_ex.named_config
+def lavagap():
+    total_timesteps=int(1e7)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-LavaGapS7-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='LavaGapPPO'
+    normalize=False
+
+@train_ex.named_config
+def distshift():
+    total_timesteps=int(1e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-DistShift1-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='PPO'
+    normalize=False
+
+@train_ex.named_config
+def distshiftv1():
+    total_timesteps=int(1e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-DistShift1-v1'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='PPO'
+    normalize=False
+
+@train_ex.named_config
+def lavagapv1():
+    total_timesteps=int(5e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-LavaGapS6-v1'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='LavaGapPPO'
+    normalize=False
+
+@train_ex.named_config
+def lavagap6():
+    total_timesteps=int(5e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-LavaGapS6-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='LavaGapPPO'
+    normalize=False
+
+
+@train_ex.named_config
+def normalgap():
+    total_timesteps=int(5e6)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-NormalGapS6-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor_small,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor_small,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='NormalGapPPO'
+    normalize=False
+
+
+@train_ex.named_config
+def lava():
+    total_timesteps=int(1e7)
+    gen_batch_size = 2048 * 8
+    env_name = 'MiniGrid-LavaCrossingS9N0-v0'
+    init_trainer_kwargs = dict()
+    init_trainer_kwargs['init_rl_kwargs'] = dict(
+            policy_class='CnnPolicy',
+            policy_kwargs={
+                'cnn_extractor': minigrid_extractor,
+        }, **DEFAULT_INIT_RL_KWARGS)
+    # Get discrim kwargs
+    init_trainer_kwargs['discrim_kwargs'] = dict(
+        build_discrim_net_kwargs = dict(
+                cnn_extractor= minigrid_extractor,
+            ),
+        reward_type='positive',
+    )
+    rollout_hint='LavaPPO'
+    normalize=False
+
+@train_ex.named_config
+def negative_reward():
+    init_trainer_kwargs = {
+        'discrim_kwargs': {
+            'reward_type': 'negative'
+        }
+    }
+
+@train_ex.named_config
+def neutral():
+    init_trainer_kwargs = {
+        'discrim_kwargs': {
+            'reward_type': 'neutral'
+        }
+    }
+
+@train_ex.named_config
+def neutral_w():
+    init_trainer_kwargs = {
+        'discrim_kwargs': {
+            'reward_type': 'neutral_w',
+            'wgan_clip': 0.8,
+        }
+    }
+
+# Neutral bounded reward (0.5 - D)
+@train_ex.named_config
+def neutral_b():
+    init_trainer_kwargs = {
+        'discrim_kwargs': {
+            'reward_type': 'neutral_b'
+        }
+    }
+
+@train_ex.named_config
+def wgan():
+    init_trainer_kwargs = {
+        'discrim_kwargs': {
+            'reward_type': 'wgan',
+            'wgan_clip'  : 0.01,
+        }
+    }
+
+@train_ex.named_config
+def clip1():
+    init_trainer_kwargs = {
+        'discrim_kwargs': {
+            'reward_type': 'wgan',
+            'wgan_clip'  : 1,
+        }
+    }
+
+# Custom Gym environment named configs
+
+@train_ex.named_config
+def two_d_maze():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  env_name = "imitation/TwoDMaze-v0"
+  rollout_hint = "two_d_maze"
+
+
+@train_ex.named_config
+def custom_ant():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  locals().update(**ANT_SHARED_LOCALS)
+  env_name = "imitation/CustomAnt-v0"
+  rollout_hint = "custom_ant"
+
+
+@train_ex.named_config
+def disabled_ant():
+  locals().update(**MUJOCO_SHARED_LOCALS)
+  locals().update(**ANT_SHARED_LOCALS)
+  env_name = "imitation/DisabledAnt-v0"
+  rollout_hint = "disabled_ant"
+
+
+# Debug configs
+
+@train_ex.named_config
+def fast():
+  """Minimize the amount of computation. Useful for test cases."""
+  total_timesteps = 10
+  n_expert_demos = 1
+  n_episodes_eval = 1
+  gen_batch_size = 2
+  disc_batch_size = 2
+  disc_minibatch_size = 2
+  show_plots = False
+  n_plot_episodes = 1
+  init_trainer_kwargs = dict(
+    parallel=False,  # easier to debug with everything in one process
+    max_episode_steps=1e2,
+    num_vec=2,
+    init_rl_kwargs=dict(nminibatches=1),
+  )
---
+++ src/imitation/scripts/generate_rollouts.py
@@ -0,0 +1,170 @@
+"""Train GAIL or AIRL and plot its output.
+
+Can be used as a CLI script, or the `train_and_plot` function can be called
+directly.
+"""
+
+import os
+import os.path as osp
+import pickle
+from typing import Optional
+
+from sacred.observers import FileStorageObserver
+import tensorflow as tf
+from stable_baselines.common.vec_env import VecEnv, VecNormalize
+
+from imitation.algorithms.adversarial import init_trainer
+import imitation.envs.examples  # noqa: F401
+from imitation.policies import serialize
+from imitation.scripts.config.generate_rollouts import train_ex
+import imitation.util as util
+import imitation.util.sacred as sacred_util
+import time
+
+
+def save(trainer, save_path):
+    """Save discriminator and generator."""
+    # We implement this here and not in Trainer since we do not want to actually
+    # serialize the whole Trainer (including e.g. expert demonstrations).
+    try:
+        trainer.discrim.save(os.path.join(save_path, "discrim"))
+        # TODO(gleave): unify this with the saving logic in data_collect?
+        # (Needs #43 to be merged before attempting.)
+        serialize.save_stable_model(os.path.join(save_path, "gen_policy"),
+                                    trainer.gen_policy,
+                                    trainer.venv_train_norm)
+    except:
+        pass
+
+
+@train_ex.main
+def train(_run,
+          _seed: int,
+          env_name: str,
+          rollout_path: str,
+          normalize: bool,
+          normalize_kwargs: dict,
+          n_expert_demos: Optional[int],
+          log_dir: str,
+          init_trainer_kwargs: dict,
+          total_timesteps: int,
+          n_episodes_eval: int,
+          init_tensorboard: bool,
+          checkpoint_interval: int,
+          dac: bool,
+          rollout_save_n_timesteps: int,
+          rollout_save_n_episodes: int,
+          num_vec: int,
+          parallel: bool,
+          max_episode_steps: Optional[int]
+          ) -> dict:
+    """Train an adversarial-network-based imitation learning algorithm.
+
+  Plots (turn on using `plot_interval > 0`):
+    - Plot discriminator loss during discriminator training steps in blue and
+      discriminator loss during generator training steps in red.
+    - Plot the performance of the generator policy versus the performance of
+      a random policy. Also plot the performance of an expert policy if that is
+      provided in the arguments.
+
+  Checkpoints:
+    - DiscrimNets are saved to f"{log_dir}/checkpoints/{step}/discrim/",
+      where step is either the training epoch or "final".
+    - Generator policies are saved to
+      f"{log_dir}/checkpoints/{step}/gen_policy/".
+
+  Args:
+    _seed: Random seed.
+    env_name: The environment to train in.
+    rollout_path: Path to pickle containing list of Trajectories. Used as
+      expert demonstrations.
+    n_expert_demos: The number of expert trajectories to actually use
+      after loading them from `rollout_path`.
+      If None, then use all available trajectories.
+      If `n_expert_demos` is an `int`, then use exactly `n_expert_demos`
+      trajectories, erroring if there aren't enough trajectories. If there are
+      surplus trajectories, then use the
+      first `n_expert_demos` trajectories and drop the rest.
+    log_dir: Directory to save models and other logging to.
+
+    init_trainer_kwargs: Keyword arguments passed to `init_trainer`,
+      used to initialize the trainer.
+    total_timesteps: The number of transitions to sample from the environment
+      during training.
+    n_episodes_eval: The number of episodes to average over when calculating
+      the average episode reward of the imitation policy for return.
+
+    plot_interval: The number of epochs between each plot. If negative,
+      then plots are disabled. If zero, then only plot at the end of training.
+    n_plot_episodes: The number of episodes averaged over when
+      calculating the average episode reward of a policy for the performance
+      plots.
+    extra_episode_data_interval: Usually mean episode rewards are calculated
+      immediately before every plot. Set this parameter to a nonnegative number
+      to also add episode reward data points every
+      `extra_episodes_data_interval` epochs.
+    show_plots: Figures are always saved to `f"{log_dir}/plots/*.png"`. If
+      `show_plots` is True, then also show plots as they are created.
+    init_tensorboard: If True, then write tensorboard logs to `{log_dir}/sb_tb`.
+
+    checkpoint_interval: Save the discriminator and generator models every
+      `checkpoint_interval` epochs and after training is complete. If 0,
+      then only save weights after training is complete. If <0, then don't
+      save weights at all.
+
+  Returns:
+    A dictionary with two keys. "imit_stats" gives the return value of
+      `rollout_stats()` on rollouts test-reward-wrapped
+      environment, using the final policy (remember that the ground-truth reward
+      can be recovered from the "monitor_return" key). "expert_stats" gives the
+      return value of `rollout_stats()` on the expert demonstrations loaded from
+      `rollout_path`.
+  """
+    total_timesteps = int(total_timesteps)
+
+    tf.logging.info("Logging to %s", log_dir)
+    os.makedirs(log_dir, exist_ok=True)
+    # try:
+    #     sacred_util.build_sacred_symlink(log_dir, _run)
+    # except Exception as e:
+    #     print("didnt build symlink")
+    # # Calculate stats for expert rollouts. Used for plot and return value.
+    # with open(rollout_path, "rb") as f:
+    #     expert_trajs = pickle.load(f)
+
+    # if n_expert_demos is not None:
+    #     assert len(expert_trajs) >= n_expert_demos
+    #     expert_trajs = expert_trajs[:n_expert_demos]
+    #
+    # # expert_stats = util.rollout.rollout_stats(expert_trajs)
+    sample_until = util.rollout.make_sample_until(rollout_save_n_timesteps,
+                                                  rollout_save_n_episodes)
+
+    with util.make_session():
+        venv = util.make_vec_env(env_name, num_vec, seed=_seed,
+                                 parallel=parallel, log_dir=log_dir,
+                                 max_episode_steps=max_episode_steps, dac=dac)
+
+        print("type of venv is: ", type(venv))
+        vec_normalize = None
+        venv = vec_normalize = VecNormalize(venv)
+        print("type of venv is: ", type(venv))
+        # time.sleep(10)
+        gen_policy_path = os.path.join(log_dir, "checkpoints", "final", "gen_policy")
+        print("gen policy path is: ", gen_policy_path)
+        time.sleep(10)
+        with serialize.load_policy('ppo2', gen_policy_path, venv) as policy:
+            print(policy)
+            print('right before: ', type(venv))
+            time.sleep(10)
+            util.rollout.save(gen_policy_path, policy, venv, sample_until)
+
+
+def main_console():
+    observer = FileStorageObserver.create(osp.join('output', 'sacred', 'train'))
+    train_ex.observers.append(observer)
+    train_ex.run_commandline()
+
+
+if __name__ == "__main__":
+    main_console()
